1c222d7f92710cda74e82c48f7da1b8e1fe38ad3
Jeffrey Rennie <rennie@google.com>
2016-04-26 16:21:55 -0700

    Confirmation sample complete.

--- appengine/mail/main.py
+++ appengine/mail/main.py
@@ -17,7 +17,7 @@ Sample application that demonstrates different ways of fetching
 URLS on App Engine
 """
 
-import logging
+import datetime
 import random
 import socket
 import string
@@ -26,14 +26,25 @@ from google.appengine.ext import ndb
 # [START mail-imports]
 import webapp2
 from google.appengine.api import mail
+
+
 # [END mail-imports]
 
 
 class UserConfirmationRecord(ndb.Model):
+    """Datastore record with email address and confirmation code."""
     user_address = ndb.StringProperty(indexed=False)
     confirmed = ndb.BooleanProperty(indexed=False, default=False)
+    timestamp = ndb.DateTimeProperty(indexed=False, auto_now_add=True)
+
 
 def create_new_user_confirmation(user_address):
+    """Create a new user confirmation.
+
+    Args:
+        user_address: string, an email addres
+
+    Returns: The url to click to confirm the email address."""
     id_chars = string.ascii_letters + string.digits
     rand = random.SystemRandom()
     random_id = ''.join([rand.choice(id_chars) for i in range(42)])
@@ -45,15 +56,15 @@ def create_new_user_confirmation(user_address):
 
 
 class UserSignup(webapp2.RequestHandler):
-    _form_html = """<html><body><form method="POST">
-        Enter your email address: <input name="email_address">
-        <input type=submit>
-        </form></body></html>"""
+    """Serves the email address sign up form."""
 
     def get(self):
         self.response.content_type = 'text/html'
-        self.response.write(self._form_html)
-    
+        self.response.write("""<html><body><form method="POST">
+        Enter your email address: <input name="email_address">
+        <input type=submit>
+        </form></body></html>""")
+
     def post(self):
         user_address = self.request.get('email_address')
 
@@ -61,10 +72,9 @@ class UserSignup(webapp2.RequestHandler):
             self.get()  # Show the form again.
         else:
             confirmation_url = create_new_user_confirmation(user_address)
-            sender_address = 'Example.com Support <support@%s>' % socket.getfqdn(socket.gethostname())
+            sender_address = 'Example.com Support <pyhelloworld-1026@appspot.gserviceaccount.com>'
             subject = 'Confirm your registration'
-            body = """
-Thank you for creating an account! Please confirm your email address by
+            body = """Thank you for creating an account! Please confirm your email address by
 clicking on the link below:
 
 %s
@@ -75,13 +85,20 @@ clicking on the link below:
 
 
 class ConfirmUserSignup(webapp2.RequestHandler):
+    """Invoked when the user clicks on the confirmation link in the email."""
+
     def get(self):
         code = self.request.get('code')
-        self.response.content_type = 'text/html' 
-        record = ndb.Key(UserConfirmationRecord, code).get()
-        self.response.write('''<html><body>
-        Confirmed %s
-        </body></html>''' % record.user_address)
+        if code:
+            record = ndb.Key(UserConfirmationRecord, code).get()
+            # 2-hour time limit on confirming.
+            if record and datetime.datetime.now() - record.timestamp < datetime.timedelta(hours=2):
+                record.confirmed = True
+                record.put()
+                self.response.content_type = 'text/plain'
+                self.response.write('Confirmed %s' % record.user_address)
+                return
+        self.response.status_int = 404
 
 
 app = webapp2.WSGIApplication([
