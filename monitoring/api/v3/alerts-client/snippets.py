f4d1ead7b7cffd333547af20e1cfde64b2cd54e4
Jeffrey Rennie <rennie@google.com>
2018-05-08 15:40:15 -0700

    enable-alert-policy

--- monitoring/api/v3/alerts-client/snippets.py
+++ monitoring/api/v3/alerts-client/snippets.py
@@ -18,16 +18,41 @@ import pprint
 
 from google.cloud import monitoring_v3
 
-def list_alert_policies():
+
+def list_alert_policies(project_name: str):
     client = monitoring_v3.AlertPolicyServiceClient()
-    policies = client.list_alert_policies(project())
+    policies = client.list_alert_policies(project_name)
     for policy in policies:
         print(policy.display_name or policy.name)
+
+
+def enable_alert_policies(project_name: str, enable, filter_: str = None):
+    client = monitoring_v3.AlertPolicyServiceClient()
+    policies = client.list_alert_policies(project_name, filter_=filter_)
+    for policy in policies:
+        if bool(enable) == policy.enabled.value:
+            print('Policy', policy.name, 'is already',
+                'enabled' if policy.enabled.value else 'disabled')
+        else:
+            policy.enabled.value = bool(enable)
+            mask = monitoring_v3.types.field_mask_pb2.FieldMask()
+            mask.paths.append('enabled')
+            client.update_alert_policy(policy, mask)
+            print('Enabled' if enable else 'Disabled', policy.name)
+
 
 class MissingProjectIdError(Exception):
     pass
 
 def project():
+    """Retreieves the project name from the environment variable.
+    
+    Raises:
+        MissingProjectIdError -- When not set.
+    
+    Returns:
+        str -- the project name
+    """
     project_id = os.environ['GCLOUD_PROJECT']
     if not project_id:
         raise MissingProjectIdError('Set the environment variable ' +
@@ -46,7 +71,29 @@ if __name__ == '__main__':
         help=list_alert_policies.__doc__
     )
 
+    enable_alert_policies_parser = subparsers.add_parser(
+        'enable-alert-policies',
+        help=enable_alert_policies.__doc__
+    )
+    enable_alert_policies_parser.add_argument(
+        '--filter',
+    )
+
+    disable_alert_policies_parser = subparsers.add_parser(
+        'disable-alert-policies',
+        help=enable_alert_policies.__doc__
+    )
+    disable_alert_policies_parser.add_argument(
+        '--filter',
+    )
+
     args = parser.parse_args()
 
     if args.command == 'list-alert-policies':
-        list_alert_policies()
+        list_alert_policies(project())
+
+    if args.command == 'enable-alert-policies':
+        enable_alert_policies(project(), enable=True, filter_=args.filter)
+
+    if args.command == 'disable-alert-policies':
+        enable_alert_policies(project(), enable=False, filter_=args.filter)
